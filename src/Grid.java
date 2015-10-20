4083635609796cb4836f87b347a6ec6051c265c8
entropyofchaos <entropyofchaos@gmail.com>
2015-10-19 23:59:58 -0400

    Finished updates to Grid.java. Added some more comments. Wrote most of State.java

--- src/Grid.java
+++ src/Grid.java
@@ -8,31 +8,32 @@ import org.apache.commons.lang3.tuple.MutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
 /**
- * Class to represent our grid world for the QLearning algorithm.
+ * Class to represent our grid mWorld for the QLearning algorithm.
  */
 public class Grid {
 
-    private Vector<String> world;
-    private int cols;
-    private int rows;
-    private Vector<Pair<Integer, Integer>> walls;
+    private Vector<String> mWorld;
+    private int mCols;
+    private int mRows;
+    private Vector<Pair<Integer, Integer>> mWalls;
 
     /**
      * Default constructor
      */
     public Grid(){
-        cols = 0;
-        rows = 0;
-        world = new Vector<>();
-        walls = new Vector<>();
+
+        mCols = 0;
+        mRows = 0;
+        mWorld = new Vector<>();
+        mWalls = new Vector<>();
     }
 
     /**
-     * Reads the grid world file into the class. Determines which locations are
-     * valid and which locations are walls.
-     * @param path The path to the grid world file
+     * Reads the grid mWorld file into the class. Determines which locations are
+     * valid and which locations are mWalls.
+     * @param path The path to the grid mWorld file
      */
-    void readFile(String path){
+    public void readFile(String path){
 
         Path filePath = Paths.get(path);
         Charset charset = Charset.forName("ISO-8859-1");
@@ -41,14 +42,14 @@ public class Grid {
             // Read all lines from a file into a list of strings, then
             // append the list of strings to our vector of strings. This
             // vector represents the lines of our map file.
-            world.addAll(Files.readAllLines(filePath, charset));
-            for (int i = 0; i < world.size(); ++i)
+            mWorld.addAll(Files.readAllLines(filePath, charset));
+            for (int i = 0; i < mWorld.size(); ++i)
             {
-                world.set(i, world.get(i).trim());
+                mWorld.set(i, mWorld.get(i).trim());
             }
 
-            rows = world.size();
-            cols = world.elementAt(0).length();
+            mRows = mWorld.size();
+            mCols = mWorld.elementAt(0).length();
 
         } catch (IOException e){
             System.out.println(e.toString());
@@ -56,22 +57,22 @@ public class Grid {
 
         // Go through all the characters in the grid map and see if we find any
         // x's. If we find one, it represents a wall, so we add it to the list
-        // of walls.
-        for (int x = 0; x < world.size(); ++x){
-            for (int y = 0; y < world.elementAt(x).length(); ++y){
-                if (world.elementAt(x).charAt(y) == 'x'){
-                    walls.add(new MutablePair<>(x, y));
+        // of mWalls.
+        for (int x = 0; x < mWorld.size(); ++x){
+            for (int y = 0; y < mWorld.elementAt(x).length(); ++y){
+                if (mWorld.elementAt(x).charAt(y) == 'x'){
+                    mWalls.add(new MutablePair<>(x, y));
                 }
             }
         }
     }
 
     /**
-     * Prints the grid world to the console.
+     * Prints the grid mWorld to the console.
      */
-    void printWorld()
+    public void printWorld()
     {
-        world.forEach(string -> System.out.println(string));
+        mWorld.forEach(string -> System.out.println(string));
     }
 
     /**
@@ -88,22 +89,22 @@ public class Grid {
         int y = loc.getLeft();
 
         // Look at cell to the right
-        if(x + 1 < cols && !walls.contains(new MutablePair<>(x + 1, y))) {
+        if(x + 1 < mCols && !mWalls.contains(new MutablePair<>(x + 1, y))) {
             adj.add(new MutablePair<>(x + 1, y));
         }
 
         // Look at cell to the left
-        if(x - 1 >= 0 && !walls.contains(new MutablePair<>(x - 1, y))) {
+        if(x - 1 >= 0 && !mWalls.contains(new MutablePair<>(x - 1, y))) {
             adj.add(new MutablePair<>(x - 1, y));
         }
 
         // Look at cell below
-        if(y + 1 < rows && !walls.contains(new MutablePair<>(x, y + 1))) {
+        if(y + 1 < mRows && !mWalls.contains(new MutablePair<>(x, y + 1))) {
             adj.add(new MutablePair<>(x, y + 1));
         }
 
         // Look at cell above
-        if(y - 1 >= 0 && !walls.contains(new MutablePair<>(x, y - 1))) {
+        if(y - 1 >= 0 && !mWalls.contains(new MutablePair<>(x, y - 1))) {
             adj.add(new MutablePair<>(x, y - 1));
         }
 
---
+++ src/State.java
@@ -0,0 +1,102 @@
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.HashMap;
+import java.util.Vector;
+
+public class State {
+
+    private MutablePair<Integer, Integer> mPosition;
+    /**
+     * Map of actions that can be taken. The key is the name of the action (left, right, up, down).
+     * The value is a pair of values where the first is the number of times this action was taken
+     * and the second value is the reward value for going that direction.
+     */
+    private HashMap<String, MutablePair<Integer, Double>> mActions;
+
+    /**
+     * The reward value for this state.
+     */
+    private double mReward;
+
+    /**
+     * Constructor for this state. A state is just a cell on the grid.
+     * @param position The X,Y position of the State
+     */
+    public State(MutablePair<Integer, Integer> position){
+
+        mPosition = position;
+        mActions = new HashMap<>();
+        mReward = 0;
+    }
+
+    /**
+     * Adds a valid transition (movement) action to the list of valid actions an agent
+     * can take along with the reward value for taking that action.
+     * @param direction The direction of the transition
+     * @param rewardValue The reward value for going the specified direction. The higher the
+     *                    reward, the more enticing the direction should be to the agent
+     */
+    public void addTransitionAction(String direction, double rewardValue){
+
+        // When adding a transition, we set the first variable of the pair (num times taken)
+        // to 0 since we have never taken this action yet and the second value to the reward
+        // value for taking this action.
+        mActions.put(direction, new MutablePair<>(0, rewardValue));
+    }
+
+    /**
+     * Set the reward for the specified action.
+     * @param direction The name of the direction for the action action to update
+     * @param rewardValue The value to set the reward to
+     */
+    public void setTransitionActionReward(String direction, double rewardValue){
+
+        // Update reward value for this direction and set the updated pair back into the hashmap
+        MutablePair<Integer, Double> oldValue = mActions.get(direction);
+        oldValue.setRight(rewardValue);
+        mActions.put(direction, oldValue);
+    }
+
+    /**
+     * Gets the action for the specified transaction.
+     * @param direction The direction to lookup
+     * @return The value of the reward
+     */
+    public MutablePair<Integer, Double> getTrasitionActionReward(String direction){
+
+        return mActions.get(direction);
+    }
+
+    /**
+     * Increment the number of times the transition action was taken for the specified direction
+     * @param direction The direction of the action to update
+     */
+    public void takeTransitionAction(String direction){
+
+        // Update the number of times  value for this direction and set the updated pair back into the hashmap
+        MutablePair<Integer, Double> oldValue = mActions.get(direction);
+        oldValue.setLeft(oldValue.getLeft() + 1);
+        mActions.put(direction, oldValue);
+    }
+
+    /**
+     * Get number of times a transition action was taken for the specified direction
+     * @param direction The direction to lookup
+     * @return The number of times the transition action was taken
+     */
+    public int numTransitionActionsTaken(String direction){
+
+        return mActions.get(direction).getLeft();
+    }
+
+    /**
+     * Gets the position of this State.
+     * @return The position of the State
+     */
+    public MutablePair<Integer, Integer> getPosition(){
+
+        return mPosition;
+    }
+}
