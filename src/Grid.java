371f3fc0f6272e401a6874ba71fc282323370484
entropyofchaos <entropyofchaos@gmail.com>
2015-10-19 22:06:14 -0400

    Added comments to Grid.java

--- src/Grid.java
+++ src/Grid.java
@@ -4,10 +4,12 @@ import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Vector;
-
 import org.apache.commons.lang3.tuple.MutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
+/**
+ * Class to represent our grid world for the QLearning algorithm.
+ */
 public class Grid {
 
     private Vector<String> world;
@@ -15,6 +17,9 @@ public class Grid {
     private int rows;
     private Vector<Pair<Integer, Integer>> walls;
 
+    /**
+     * Default constructor
+     */
     public Grid(){
         cols = 0;
         rows = 0;
@@ -22,6 +27,11 @@ public class Grid {
         walls = new Vector<>();
     }
 
+    /**
+     * Reads the grid world file into the class. Determines which locations are
+     * valid and which locations are walls.
+     * @param path The path to the grid world file
+     */
     void readFile(String path){
 
         Path filePath = Paths.get(path);
@@ -44,6 +54,9 @@ public class Grid {
             System.out.println(e.toString());
         }
 
+        // Go through all the characters in the grid map and see if we find any
+        // x's. If we find one, it represents a wall, so we add it to the list
+        // of walls.
         for (int x = 0; x < world.size(); ++x){
             for (int y = 0; y < world.elementAt(x).length(); ++y){
                 if (world.elementAt(x).charAt(y) == 'x'){
@@ -53,26 +66,43 @@ public class Grid {
         }
     }
 
+    /**
+     * Prints the grid world to the console.
+     */
     void printWorld()
     {
         world.forEach(string -> System.out.println(string));
     }
 
+    /**
+     * Helper function to retrieve all valid adjacent cells to the current cell.
+     * All adjacent cells that would be an invalid location, such as a wall are
+     * not added to the vector.
+     * @param loc The location you are looking at
+     * @return A vector of valid adjacent cells
+     */
     Vector<Pair<Integer, Integer>> adjacent(Pair<Integer, Integer> loc){
 
         Vector<Pair<Integer, Integer>> adj = new Vector<>();
         int x = loc.getLeft();
         int y = loc.getLeft();
 
+        // Look at cell to the right
         if(x + 1 < cols && !walls.contains(new MutablePair<>(x + 1, y))) {
             adj.add(new MutablePair<>(x + 1, y));
         }
+
+        // Look at cell to the left
         if(x - 1 >= 0 && !walls.contains(new MutablePair<>(x - 1, y))) {
             adj.add(new MutablePair<>(x - 1, y));
         }
+
+        // Look at cell below
         if(y + 1 < rows && !walls.contains(new MutablePair<>(x, y + 1))) {
             adj.add(new MutablePair<>(x, y + 1));
         }
+
+        // Look at cell above
         if(y - 1 >= 0 && !walls.contains(new MutablePair<>(x, y - 1))) {
             adj.add(new MutablePair<>(x, y - 1));
         }
